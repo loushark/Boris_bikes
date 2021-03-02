6bbc43041ddcdfa1d66a1f33b440f284c67c25e2
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-03-02 15:56:27 +0000

    We setup Rspec
    Co-Authored-By: Natalia Sherchenkova <77491121+TataSher@users.noreply.github.com>

---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,14 @@
+require 'boris_bikes.rb'
+
+# Set up the spec file to describe a DockingStation
+# Run RSpec from the Command Line
+# Explain to your pair partner the difference between this error and the error you saw before.
+
+# describe 'solution' do
+#     it 'outputs "I" when the input is 1' do
+#         expect(solution(1)).to eq 'I'
+#     end
+# end
+
+describe 'DockingStation' do
+end
