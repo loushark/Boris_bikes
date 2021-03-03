e5286ef913795439276f28a5c0fff14af853e28e
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-03-03 16:54:33 +0000

    Added is_there_a_bike? and dock method and tested both
    Co-Authored-By: Lou Reade <39278690+loushark@users.noreply.github.com>

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,11 +1,31 @@
 class DockingStation
+  attr_accessor :name, :storage
 
-  def release_bike
-    Bike.new
+  def initialize(name)
+    @name = name
+    @storage = []
   end
+
+  def dock(bike)
+    self.storage << bike.name
+  end
+
+  def is_there_a_bike?
+    self.storage == [] ? false : self.storage[0]
+  end
+
+  def release_bike(name)
+    Bike.new(name)
+  end
+
 end
 
 class Bike
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 
   def working?
     true
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -5,16 +5,41 @@ require 'boris_bikes.rb'
 # I'd like a docking station to release a bike.
 
 describe 'DockingStation' do
-  let(:docking_station) {DockingStation.new}
+  let(:central) { DockingStation.new 'central'}
 
   describe 'release_bike' do
     it 'returns a new instance of the Bike class' do
       expect do
-        unicycle = docking_station.release_bike
+        unicycle = central.release_bike('unicycle')
         print unicycle.working?
       end.to output("true").to_stdout
-
     end
   end
 
+  describe 'dock' do
+    it "stores a bike in an instance of a docking station" do
+      expect do
+        unicycle = central.release_bike('unicycle')
+        central.dock(unicycle)
+        print central.storage[0]
+      end.to output("unicycle").to_stdout
+    end
+  end
+
+  describe 'is_there_a_bike?' do
+
+    it "checks if there is a bike in the instance variable of a docking station instance" do
+      expect do
+        unicycle = Bike.new('unicycle')
+        print central.is_there_a_bike?
+      end.to output("false").to_stdout
+    end
+
+
+    it "returns a bike name if there is a bike in storage" do
+      unicycle = Bike.new('unicycle')
+      central.dock(unicycle)
+      expect(central.is_there_a_bike?).to eq("unicycle")
+    end  
+  end
 end
