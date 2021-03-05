c4aa56f006686432bbded36f18c554b1298432f3
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-03-05 15:39:33 +0000

    Increased capacity to 20
    Co-Authored-By: LizTruman <77003338+eatruman@users.noreply.github.com>

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,33 +1,33 @@
 class DockingStation
   attr_accessor :name, :storage, :capacity
 
-  def initialize(name, storage=[], capacity=1)
+  def initialize(name, storage=[], capacity=20)
     @name = name
     @storage = storage
     @capacity = capacity
   end
 
   def dock(bike)
-    if self.storage.length == self.capacity
+    if storage.length == capacity
       fail "Cannot dock bike, at capacity"
     else
-      self.storage << bike.name
+      storage << bike.name
     end
   end
 
   def is_there_a_bike?
-    self.storage != [] ? true : false
+    storage != [] ? true : false
   end
 
   def release_bike
-    if self.is_there_a_bike?
-      self.storage[0]
+    if is_there_a_bike?
+      storage[0]
     else
       fail "No bikes available"
     end
   end
-
 end
+
 
 class Bike
 attr_accessor :name
